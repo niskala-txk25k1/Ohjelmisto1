dce4a47f6711480ebf7cdafca47460354fc365c5
Niskala <Daniel.Niskala@metropolia.fi>
2025-01-18 11:37:35 +0200

    Käytä tasajakoa int-castin sijaan

--- moduuli7/ohjelma1.py
+++ moduuli7/ohjelma1.py
@@ -4,5 +4,5 @@ import sys
 month = int(sys.argv[1] if len(sys.argv)==2 else input("Kuukauden numero? "))
 
 seasons = "talvi", "kevät", "kesä", "syksy"
-index = (month%12) / 3
-print( seasons[int(index)] )
+index = (month%12) // 3
+print(seasons[index])
